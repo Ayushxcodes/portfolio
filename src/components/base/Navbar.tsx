fde5208c3c54eeb1af4406b8389e264d6dc9cdb7
Ayush Pandey <en22cs301251@medicaps.ac.in>
2025-12-08 00:14:35 +0530

    done_navbar

--- src/components/base/Navbar.tsx
+++ src/components/base/Navbar.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import CardNav from '@/components/CardNav'
+
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const items = [
+    {
+      label: "About",
+      bgColor: "#0D0716",
+      textColor: "#fff",
+      links: [
+        { label: "Company", href: "/about/company", ariaLabel: "About Company" },
+        { label: "Careers", href: "/about/careers", ariaLabel: "About Careers" }
+      ]
+    },
+    {
+      label: "Projects", 
+      bgColor: "#170D27",
+      textColor: "#fff",
+      links: [
+        { label: "Featured", href: "/projects/featured", ariaLabel: "Featured Projects" },
+        { label: "Case Studies", href: "/projects/case-studies", ariaLabel: "Project Case Studies" }
+      ]
+    },
+    {
+      label: "Contact",
+      bgColor: "#271E37", 
+      textColor: "#fff",
+      links: [
+        { label: "Email", href: "mailto:info@example.com", ariaLabel: "Email us" },
+        { label: "Twitter", href: "https://twitter.com/yourprofile", ariaLabel: "Twitter" },
+        { label: "LinkedIn", href: "https://linkedin.com/in/yourprofile", ariaLabel: "LinkedIn" }
+      ]
+    }
+  ];
 
-export default Navbar
+  return (
+    <CardNav
+      logo={"/Logo.png"}
+      logoAlt="Company Logo"
+      items={items}
+      baseColor="#fff"
+      menuColor="#000"
+      buttonBgColor="#111"
+      buttonTextColor="#fff"
+      ease="power3.out"
+    />
+  );
+};
+
+export default Navbar;
